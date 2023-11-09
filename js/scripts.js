fca2fab5a6a0a311b3294e6e32f90db5ed1b501a
joaocordao <jp.cordao@gmail.com>
2023-11-09 18:34:03 +0100

    Housing the Pokémon list within an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
 // array of pokemons
-let pokemonList = [
-    { name: 'Fearow', height: 1.2 , types: ['flying', 'normal']},
-    { name: 'Bulbasaur', height: 0.7 , types: ['grass', 'poison']},
-    { name: 'Nidoking', height: 1.4 , types: ['ground', 'poison']}
-];
+let pokemonRepository = (function () {
 
-pokemonList.forEach(function(pokemonList) {
-    if (pokemonList.height < 1.3){
-    console.log(pokemonList.name + " (height: " + pokemonList.height + " )");
+    let pokemonList = [
+        { name: 'Fearow', height: 1.2 , types: ['flying', 'normal']},
+        { name: 'Bulbasaur', height: 0.7 , types: ['grass', 'poison']},
+        { name: 'Nidoking', height: 1.4 , types: ['ground', 'poison']}
+    ];
+
+    function getAll () {
+        return pokemonList;
+    } 
+
+    function add (pokemon) {
+        pokemonList.push(pokemon);
     }
-    else if (pokemonList.height >= 1.3){
-    console.log(pokemonList.name + " (height: " + pokemonList.height + " )" + " - Wow, that's big!");
+    return {
+        getAll: getAll,
+        add: add
     }
-});
+})()
+
+    console.log(pokemonRepository.getAll());
+    console.log(pokemonRepository.add());
+    
+    pokemonList.forEach(function(pokemonList) {
+        if (pokemonList.height < 1.3){
+        console.log(pokemonList.name + " (height: " + pokemonList.height + " )");
+        }
+        else if (pokemonList.height >= 1.3){
+        console.log(pokemonList.name + " (height: " + pokemonList.height + " )" + " - Wow, that's big!");
+        }
+    });
